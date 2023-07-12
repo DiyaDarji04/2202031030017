5c1c712841855173679075ef9ea94f134229c76c
DiyaDarji04 <137033575+DiyaDarji04@users.noreply.github.com>
2023-07-12 17:52:18 +0530

    Add files via upload

---
+++ Day 16/stringfunction.php
@@ -0,0 +1,17 @@
+<?php
+echo "String Length function using php"."<br>";
+echo strlen("hello world"); 
+echo "<br>";
+echo "String word count function using php"."<br>";
+echo str_word_count("Diyaa"); // outputs 2
+echo "<br>";
+echo "String reverse function using php"."<br>";
+echo strrev("diyaa!");
+echo "<br>";
+echo "String position function using php"."<br>";
+echo strpos("Hello world!", "world");
+echo "<br>";
+echo "String replace function using php"."<br>";
+echo str_replace("world", "Diya", "Hello world!");
+
+?>
